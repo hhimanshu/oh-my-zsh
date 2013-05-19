16ad8ac16267d715a7b6fb7a6cf45867c668cdae
Harit Himanshu <harit.himanshu@yahoo.in>
2013-05-19 09:23:08 -0700

    modified alias.sh

--- alias.zsh
+++ alias.zsh
@@ -10,6 +10,7 @@ alias path='echo -e ${PATH//:/\\n}'
 alias du='du -kh'       # Makes a more readable output.
 alias df='df -kTh'
 alias lt='ls -ltr'
+alias reload='source ~/.zshrc'
 
 # Applications to run from command-line
 alias subl="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
